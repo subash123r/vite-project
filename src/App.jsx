a69f60342142f685b4f6f3a32792d159b263201a
subash123r <rssubash96@gmail.com>
2024-04-06 15:01:51 +0530

    firstreactapplication

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+   <Greet />
+    </>
+  );
+}
+
+function Greet(){
+  return(
+    <div>
+      <h1>welcome to react</h1>
+    </div>
+  );
+}
+export default App;
